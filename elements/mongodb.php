b03efe6218b8cb18326b1fee50cfdd80b44de210
NoSoussaille <marwane.belhadi@hotmail.com>
2025-01-28 14:22:04 +0100

    configuration de déploiement

--- elements/mongodb.php
+++ elements/mongodb.php
@@ -1,10 +1,23 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+
 try {
-    $mongoClient = new MongoDB\Client("mongodb+srv://marwanebelhadi:FSdtheTbPlaokgX3@cluster0.pdc90.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    // Récupérer l'URI MongoDB depuis les variables d'environnement
+    $mongoUri = getenv('MONGODB_URI');
+
+    // Vérifiez si la variable d'environnement est définie
+    if (!$mongoUri) {
+        throw new Exception('La variable d\'environnement MONGODB_URI n\'est pas définie.');
+    }
+
+    // Connexion au client MongoDB
+    $mongoClient = new MongoDB\Client($mongoUri);
+
+    // Sélection de la base de données et de la collection
     $db = $mongoClient->Cuisine; // Base de données
     $collectionAvis = $db->avis; // Collection des avis
+
 } catch (Exception $e) {
-    die("Erreur de connexion à MongoDB : " . $e->getMessage());
+    die("Erreur de connexion à MongoDB : " . $e->getMessage());
 }
 ?>
